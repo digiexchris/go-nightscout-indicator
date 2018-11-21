ccd270ea826357c028e3888b49124950501fbf62
Chris Chatelain <cchatelain@boldcommerce.com>
2018-11-21 15:34:49 -0600

    Add in two more direction possibilities

--- direction/direction.go
+++ direction/direction.go
@@ -1,18 +1,19 @@
 package direction
 
 var directions = map[string]string{
-	"NONE":          "⇼",
-	"DoubleUp":      "⇈",
-	"SingleUp":      "↑",
-	"FortyFiveUp":   "↗",
-	"Flat":          "→",
-	"FortyFiveDown": "↘",
-	"SingleDown":    "↓",
-	"DoubleDown":    "⇊",
+	"NONE":              "⇼",
+	"DoubleUp":          "⇈",
+	"SingleUp":          "↑",
+	"FortyFiveUp":       "↗",
+	"Flat":              "→",
+	"FortyFiveDown":     "↘",
+	"SingleDown":        "↓",
+	"DoubleDown":        "⇊",
+	"NOT COMPUTABLE":    "-",
+	"RATE OUT OF RANGE": "⇕",
 }
 
-//const "NOT COMPUTABLE" = "-"
-//const "RATE OUT OF RANGE" = "⇕"
+//todo make this modifyable by config file so people can put custom emoji in here or something fun
 
 func GetDirectionForTrend(trend string) string {
 	if val, ok := directions[trend]; ok {
